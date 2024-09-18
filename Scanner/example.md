6bb2b9be4fd5e4c65c967a88dc1dfbd74c5118eb
jrhauser <jrh00045@mix.wvu.edu>
2024-09-18 11:47:06 -0400

    update example and work on lookup table

--- Scanner/example.md
+++ Scanner/example.md
@@ -5,8 +5,8 @@ Given the input '' x = x + 1; ''
 
 The output should be:
 * 'x', indentifier
-* '=', assignment
+* '=',
 * 'x', identifier
-* '+', addition
+* '+',
 * '1', literal
-* ';', EOE (end of Expression)
+* ';'
--- Scanner/src/Scanner.java
+++ Scanner/src/Scanner.java
@@ -1,8 +1,24 @@
+
+import java.util.HashMap;
+
+
 public class Scanner {
-    
     public static void main(String[] args) throws Exception {
-        int state;
-            
+        var lookup = new HashMap<Character, Integer>();
+        char[] alpha = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                        'A', 'B',  'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        int i = 0;
+        for (char c: alpha) {
+            lookup.put(alpha[i], i);
+            i++;
+        }
+        lookup.put('+', 52);
+        lookup.put('-', 52);
+        lookup.put('/', 52);
+        lookup.put('%', 52);
+        lookup.put('*', 52);
+        lookup.put('+', 52);
+        int state = 0;
         String strInput = "x = x + 1;";
         char[] input = strInput.toCharArray();
         for (int val : input) {
