fb630bb88c9aebfea6ca4cb504d1702bf3c04841
jrhauser <jrh00045@mix.wvu.edu>
2024-10-15 14:37:12 -0400

    Updated phase 1 code and phase 2 method stubs

---
+++ Scanner/src/Phase2Parser.java
@@ -0,0 +1,50 @@
+public class Phase2Parser {
+    
+    void Program() {
+
+    }
+
+    void Statement() {
+
+    }
+
+    void Else() {
+
+    }
+    void Declaration() {
+
+    } 
+
+    void Assignment() {
+
+    }
+
+    void Type() {
+    }
+
+    void Name() {
+
+    }
+    void Value() {
+
+    } 
+    void Number() {
+
+    } 
+    void Integer() {
+
+    }
+    void Boolean() {
+
+    } 
+    void Operator() {
+
+    }
+    void Comparison() {
+
+    }
+
+    void Condition() {
+        
+    }
+}
