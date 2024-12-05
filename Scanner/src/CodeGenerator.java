89052ad9e4459e2c1977dec9c9591b87dac30abc
emsitler <ems00030@mix.wvu.edu>
2024-12-05 15:56:31 -0500

    Added a method for SUB that forms binary instruction with an Integer
    bitList is an integer that other integers get added to and shifted to the right positions. Choose this or the BitSet strategy to make future methods

--- Scanner/src/CodeGenerator.java
+++ Scanner/src/CodeGenerator.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
+import java.lang.Math;
 
 
 public class CodeGenerator {
@@ -9,7 +10,7 @@ public class CodeGenerator {
     static int mem = 4000;
     static int reg = 1;
     public static void main(String args[]) { 
-
+        System.out.println(subAtom());
     } 
     public static void codeGen(ArrayList<List<Object>> atoms) {
         
@@ -34,15 +35,38 @@ public class CodeGenerator {
 
     private String addAtom() {
         BitSet bits = new BitSet();
+        //Setting op code
         bits.set(3);
+        //setting cmp, r1 bits
         for (int i = 4; i > 0; i--) {
            if ((reg >> i & 1) == 1)
                 bits.set(i + 7);
         }
+        //setting 20 bits for s2
         for (int i = 20; i > 0; i--) {
             if ((labelTable[pc + 500] >> i & 1) == 1)
                  bits.set(i + 31);
          }
         return bits.toString();
+    }
+
+    public static String subAtom() {
+        //Unfinished. Once memory declaration is figured out there will need to be 2 load instructions to replace a and b declarations, and a store op after
+        Integer bitList = 0;
+        int opCode = 2;
+        //a - b = result
+        int b = 12;
+        int a = 15;
+
+        //setting opCode
+        bitList += (opCode << 28) ;
+        //setting cmp
+        bitList += (0 << 24) ;
+        //setting r1
+        bitList += (a << 20);
+        //setting memory s2
+        bitList += (b << 16);
+        //adds leading zeroes depending on op code.
+        return "00" + Integer.toBinaryString(bitList);
     }
 }
