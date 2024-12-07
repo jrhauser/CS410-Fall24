db1cc492ffaaa2606d912f24d415ea20bcfcbabd
jrhauser <jrh00045@mix.wvu.edu>
2024-12-06 23:55:14 -0500

    ready to debug code gen

--- Compiler/src/CodeGenerator.java
+++ Compiler/src/CodeGenerator.java
@@ -53,6 +53,8 @@ public class CodeGenerator {
             if (atom.get(1).equals("LBL")) {
                 labelTable.put(atom.get(6).toString(), pc);
             } else if (atom.get(1).equals("MOV")) {
+                labelTable.put(atom.get(2).toString(), mem);
+                mem += 8;
                 labelTable.put(atom.get(5).toString(), mem);
                 mem += 8;
                 pc += 2;
@@ -74,163 +76,77 @@ public class CodeGenerator {
     private static void tstAtom(List<Object> atom) {
         loadWord(atom.get(2).toString());
         Integer bitList = 0;
-        int opCode = 6; // LOD
-        bitList += (opCode << 28); // opcode = 0111
+        int opCode = 6; // CMP
+        bitList += (opCode << 28); // opcode = 0110
         bitList += (Integer.parseInt(atom.get(5).toString()) << 24); // cmp = comparison number
         bitList += (reg);
         bitList += (labelTable.get(atom.get(3)) << 20);
         code.add("0" + Integer.toBinaryString(bitList));
-        bitList = 0;
-        opCode = 5;
-        bitList += (opCode << 28); // opcode = 0101
-        bitList += (Integer.parseInt(atom.get(5).toString()) << 24); // cmp = 0000
-        bitList += (reg);
-        bitList += (labelTable.get(atom.get(3)) << 20);
         jmp(atom.get(6).toString());
     }
 
     public static void addAtom(List<Object> atom) {
-        loadWord(atom.get(2).toString()); //lw first number
-        loadWord(atom.get(3).toString()); //lw second number
-
-        BitSet bits = new BitSet(); //add instruction
-        bits.set(3);
-        for (int i = 4; i > 0; i--) {
-            if ((reg >> i & 1) == 1)
-                bits.set(i + 7);
-        }
-        // setting 20 bits for s2
-        for (int i = 20; i > 0; i--) {
-            if ((labelTable.get(atom.get(3)) >> i & 1) == 1)
-                bits.set(i + 31);
-        }
-        code.add(bits.toString());
+        loadWord(atom.get(2).toString()); // lw first number // lw second number
+        Integer bitList = 0;
+        int opCode = 1; // ADD
+        bitList += (opCode << 28); // opcode = 0001
+        bitList += (0 << 24); // cmp = 0
+        bitList += (reg);
+        bitList += (labelTable.get(atom.get(3)) << 20);
+        code.add("000" + Integer.toBinaryString(bitList));
+        storeWord(atom.get(4).toString());
     }
 
     public static void subAtom(List<Object> atom) {
-        loadWord(atom.get(2).toString()); //lw first number
-        loadWord(atom.get(3).toString()); //lw second number
-
-        BitSet bits = new BitSet();
-        bits.set(2); // 0010
-        for (int i = 4; i > 0; i--) {
-            if ((reg >> i & 1) == 1)
-                bits.set(i + 7);
-        }
-        for (int i = 20; i > 0; i--) {
-            if ((labelTable.get(atom.get(3)) >> i & 1) == 1)
-                bits.set(i + 31);
-        }
-        code.add(bits.toString());
+        loadWord(atom.get(2).toString()); // lw first number // lw second number
+        Integer bitList = 0;
+        int opCode = 2; // SUB
+        bitList += (opCode << 28); // opcode = 0010
+        bitList += (0 << 24); // cmp = 0
+        bitList += (reg);
+        bitList += (labelTable.get(atom.get(3)) << 20);
+        code.add("00" + Integer.toBinaryString(bitList));
+        storeWord(atom.get(4).toString());
     }
 
     public static void mulAtom(List<Object> atom) {
-        loadWord(atom.get(2).toString()); //lw first number
-        loadWord(atom.get(3).toString()); //lw second number
-
-        BitSet bits = new BitSet();
-        bits.set(2, 3); // 0011
-        for (int i = 4; i > 0; i--) {
-            if ((reg >> i & 1) == 1)
-                bits.set(i + 7);
-        }
-        for (int i = 20; i > 0; i--) {
-            if ((labelTable.get(atom.get(3)) >> i & 1) == 1)
-                bits.set(i + 31);
-        }
-        code.add(bits.toString());
+        loadWord(atom.get(2).toString()); // lw first number // lw second number
+        Integer bitList = 0;
+        int opCode = 3; // MUL
+        bitList += (opCode << 28); // opcode = 0011
+        bitList += (0 << 24); // cmp = 0
+        bitList += (reg);
+        bitList += (labelTable.get(atom.get(3)) << 20);
+        code.add("00" + Integer.toBinaryString(bitList));
+        storeWord(atom.get(4).toString());
     }
 
     public static void divAtom(List<Object> atom) {
-        loadWord(atom.get(2).toString()); //lw first number
-        loadWord(atom.get(3).toString()); //lw second number
-        
-        BitSet bits = new BitSet();
-        bits.set(1); // 0100
-        for (int i = 4; i > 0; i--) {
-            if ((reg >> i & 1) == 1)
-                bits.set(i + 7);
-        }
-        for (int i = 20; i > 0; i--) {
-            if ((labelTable.get((String) atom.get(3)) >> i & 1) == 1)
-                bits.set(i + 31);
-        }
-        code.add(bits.toString());
+        loadWord(atom.get(2).toString()); // lw first number // lw second number
+        Integer bitList = 0;
+        int opCode = 4; // DIV
+        bitList += (opCode << 28); // opcode = 0100
+        bitList += (0 << 24); // cmp = 0
+        bitList += (reg);
+        bitList += (labelTable.get(atom.get(3)) << 20);
+        code.add("0" + Integer.toBinaryString(bitList));
+        storeWord(atom.get(4).toString());
     }
 
     public static void jmpAtom(List<Object> atom) {
-        BitSet bits = new BitSet();
-        // Gen CMP 0110
-        bits.set(1, 2);
-        for (int i = 4; i > 0; i--) {
-            if ((reg >> i & 1) == 1)
-                bits.set(i + 7);
-        }
-        for (int i = 20; i > 0; i--) {
-            if ((labelTable.get((String) atom.get(3)) >> i & 1) == 1)
-                bits.set(i + 31);
-        }
-        code.add(bits.toString());
-        bits.clear();
-
-        // Gen JMP 0101
-        bits.set(1);
-        bits.set(3);
-        for (int i = 4; i > 0; i--) {
-            if ((reg >> i & 1) == 1)
-                bits.set(i + 7);
-        }
-        for (int i = 20; i > 0; i--) {
-            if ((labelTable.get((String) atom.get(3)) >> i & 1) == 1)
-                bits.set(i + 31);
-        }
-        code.add(bits.toString());
-
+        Integer bitList = 0;
+        int opCode = 6; // CMP
+        bitList += (opCode << 28); // opcode = 0110
+        bitList += (0 << 24); // cmp = comparison number
+        bitList += (reg);
+        bitList += (0 << 20);
+        code.add("0" + Integer.toBinaryString(bitList));
+        jmp(atom.get(6).toString());
     }
 
     public static void movAtom(List<Object> atom) {
-        BitSet bits = new BitSet();
-        // Gen LOD 0111
-        bits.set(1, 3);
-        for (int i = 4; i > 0; i--) {
-            if ((reg >> i & 1) == 1)
-                bits.set(i + 7);
-        }
-
-        // Register r
-        for (int i = 4; i > 0; i--) {
-            if ((reg >> i & 1) == 1)
-                bits.set(i + 11);
-        }
-
-        // Memory
-        for (int i = 20; i > 0; i--) {
-            if ((labelTable.get((String) atom.get(3)) >> i & 1) == 1)
-                bits.set(i + 31);
-        }
-
-        code.add(bits.toString());
-        bits.clear();
-
-        // Gen STO 1000
-        bits.set(0);
-        for (int i = 4; i > 0; i--) {
-            if ((reg >> i & 1) == 1)
-                bits.set(i + 7);
-        }
-
-        // Register r
-        for (int i = 4; i > 0; i--) {
-            if ((reg >> i & 1) == 1)
-                bits.set(i + 11);
-        }
-
-        // Memory
-        for (int i = 20; i > 0; i--) {
-            if ((labelTable.get((String) atom.get(3)) >> i & 1) == 1)
-                bits.set(i + 31);
-        }
-        code.add(bits.toString());
+        loadWord(atom.get(2).toString());
+        storeWord(atom.get(4).toString());
     }
 
     public static void loadWord(String identifier) {
@@ -246,7 +162,7 @@ public class CodeGenerator {
 
     private static void storeWord(String identifier) {
         Integer bitList = 0;
-        int opCode = 8; // LOD
+        int opCode = 8; // STO
         bitList += (opCode << 28); // opcode = 1000
         bitList += (0 << 24); // cmp = 0000
         bitList += (reg);
